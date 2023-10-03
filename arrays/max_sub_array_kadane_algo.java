cea8a027ef4db30e0b8eed3f3fe655ca6f91f116
Yash Saxena <yashsaxena8055@gmail.com>
2023-10-03 20:25:06 +0530

    Update max_sub_array_kadane_algo.java

--- arrays/max_sub_array_kadane_algo.java
+++ arrays/max_sub_array_kadane_algo.java
@@ -1,1 +1,40 @@
 
+
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+    
+    private static int getMaxSubArraySum(int arr[]){
+        //int arr []= {-2, -3, 4, -1, -2, 1, 5, -3};
+        int sum =0;
+        int max  = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length ;i++){
+            sum += arr[i];
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum =0;
+            }
+            
+        }
+        return max;
+    }
+    
+    private static void display(int[] arr){
+        
+    for(int i = 0; i < arr.length ; ++i)
+        System.out.print(arr[i] + " ");
+          
+         System.out.println();
+    }
+    
+	public static void main(String[] args) {
+		int arr []= {-2, -3, 4, -1, -2, 1, 5, -3};
+	    System.out.println("Max sub array sum : "+getMaxSubArraySum(arr));
+		
+        
+	}
+}
